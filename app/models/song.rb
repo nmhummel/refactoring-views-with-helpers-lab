809f4eb89b01f9cabb00a2fcf607685cf4ff0c52
nmhummel <natalie.hummel@gmail.com>
2021-01-11 16:15:46 -0600

    commit tree a0d3158c0ec8187aff124c8f3305ad7adb195df2

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
   end
+
+  # Write the code for #artist_name and #artist_name= so that an Artist 
+  # can be retrieved from, and associated with, a Song instance
+
 end
